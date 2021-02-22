de16d221f50768c4c29e613b4c5b1a6f8aa919ed
ayo <ayo@ciklum.com>
2021-02-22 19:34:15 +0500

    testing frontend changes

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 } else {
     app.use(express.static(path.join(__dirname, 'public')));
 }
