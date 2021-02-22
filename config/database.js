589fde2e716df97f9a99cc2172b5bdb8462c7a6f
ayo <ayo@ciklum.com>
2021-02-22 18:42:38 +0500

    database changes

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ var pg = require('pg');
 const { Client } = require('pg');
 require('dotenv').config();
 const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.DB_USER_PROD,
+    host: process.env.DB_HOST_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    port: process.env.DB_PORT_PROD,
 });
 
 client.connect().then((result) => console.log('Database connection successful')).catch((err) => console.log(err));
