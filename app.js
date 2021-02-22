08c3872da10f8a1d9a420d704a621403b7dbbd08
ayo <ayo@ciklum.com>
2021-02-23 01:08:42 +0500

    testing react app on heroku

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-if(process.env.NODE_ENV === 'production'){
-    
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        app.use(express.static(path.join(__dirname, 'client/build')));
-    });
-} else {
-    app.use(express.static(path.join(__dirname, 'public')));
-}
 
 
 
@@ -31,10 +22,20 @@ app.use(cors({
  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
 }));
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+} else {
+    app.use(express.static(path.join(__dirname, 'public')));
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRoutes);
 app.use('/chat', chatRoutes);
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
+})
 
 
 
