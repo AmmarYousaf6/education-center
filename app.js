487ccdb89fb0a98b8e90b8f2a03b2a05d4fe0fd6
ayo <ayo@ciklum.com>
2021-02-22 19:51:12 +0500

    testing frontend changes

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ if(process.env.NODE_ENV === 'production'){
     //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     // });
 } else {
-    // app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 }
-}
+
 
 
 app.use(cors({
